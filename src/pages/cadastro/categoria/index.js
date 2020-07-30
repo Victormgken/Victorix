d287d14709d950ed7dced7995af44c93c57d974f
Victormgken <victormgken@gmail.com>
2020-07-30 13:10:51 -0300

    Ajax, eslint,

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,44 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import FormField from '../../../components/FormField'
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
+  const valoresInicias = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
   const [categorias, setCategorias] = useState([]);
-  const valoresInicias ={
-    nome:'Categoria',
-    descricao: 'Descrição',
-    cor: '#000',
-  }
   const [values, setValues] = useState(valoresInicias);
 
   function setValue(key, value) {
     setValues({
       ...values,
       [key]: value,
-    })
-  };
+    });
+  }
 
-  function functionHandler(info){
+  function functionHandler(info) {
     setValue(
-      info.target.getAttribute('name'), 
-      info.target.value
-      );
-  };
-  
-  return(
+      info.target.getAttribute('name'),
+      info.target.value,
+    );
+  }
+
+  useEffect(() => {
+    const Url = 'http://localhost:8080/categorias';
+    fetch(Url)
+      .then(async (serverResponse) => {
+        const response = await serverResponse.json();
+        setCategorias([
+          ...response,
+        ]);
+      });
+  });
+
+  return (
     <PageDefault>
-        <h1>Cadastro de Categoria: {values.nome}</h1>
-        
-        <form onSubmit = {function handleSubmit(info){
-          info.preventDefault();
-          setCategorias([
-            ...categorias,
-            values
-          ]);
-        }}>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(info) {
+        info.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+      }}
+      >
         <div>
-          <FormField 
+          <FormField
             label="Nome da Categoria"
             type="text"
             name="nome"
@@ -46,15 +62,15 @@ function CadastroCategoria() {
             onChange={functionHandler}
           />
 
-          <FormField 
+          <FormField
             label="Descrição da Categoria"
-            type="text"
+            type="textarea"
             name="descricao"
             value={values.descricao}
             onChange={functionHandler}
           />
 
-          <FormField 
+          <FormField
             label="Cor da Categoria"
             type="color"
             name="cor"
@@ -63,25 +79,23 @@ function CadastroCategoria() {
           />
         </div>
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
-        <ul>
-          {categorias.map((categoria, indice) => {
-          return (
-            <li key ={`${categoria}${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-          })}
-        </ul>
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
-        <Link to ="/">
-            Ir para home
-        </Link>
-    </PageDefault>    
-    )
-  }
-  
-  export default CadastroCategoria;
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
